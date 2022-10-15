6860485495de81dadc10296f9c24e9df85e6bda4
tegarswasono <tegar.s@weefer.co.id>
2022-10-15 21:21:04 +0700

    first commit

---
+++ LearnBasicAuth/BasicAuth/BasicAuthenticationHandler.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace LearnBasicAuth.BasicAuth
+{
+    public class BasicAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        private Dictionary<string, string> userRegistered = new Dictionary<string, string>()
+        {
+            { "user1", "password1" },
+            { "user2", "password2" },
+            { "user3", "password3" }
+        };
+
+        public BasicAuthenticationHandler(
+            IOptionsMonitor<AuthenticationSchemeOptions> options,
+            ILoggerFactory logger,
+            UrlEncoder encoder,
+            ISystemClock clock)
+            : base(options, logger, encoder, clock)
+        {
+            
+        }
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            var endpoint = Context.GetEndpoint();
+            if (endpoint?.Metadata?.GetMetadata<IAllowAnonymous>() != null)
+            {
+                return AuthenticateResult.NoResult();
+            }
+            if (!Request.Headers.ContainsKey("Authorization"))
+            {
+                return AuthenticateResult.Fail("Missing Authorization Header");
+            }
+
+            try
+            {
+                //kalau mau rubah dari basic auth menjadi bearer token tinggal rubah aja di bagian sini
+                var authHeader = AuthenticationHeaderValue.Parse(Request.Headers["Authorization"]);
+                var credentialBytes = Convert.FromBase64String(authHeader.Parameter);
+                var credentials = Encoding.UTF8.GetString(credentialBytes).Split(new[] { ':' }, 2);
+                var username = credentials[0];
+                var password = credentials[1];
+
+                var isExist = userRegistered.Any(x => x.Key == username && x.Value == password);
+                if (!isExist)
+                {
+                    return AuthenticateResult.Fail("Invalid Authorization Header");
+                }
+                var claims = new[]
+                {
+                    new Claim(ClaimTypes.Name, username)
+                };
+                var identity = new ClaimsIdentity(claims, Scheme.Name);
+                var principal = new ClaimsPrincipal(identity);
+                var ticket = new AuthenticationTicket(principal, Scheme.Name);
+
+                return AuthenticateResult.Success(ticket);
+            }
+            catch
+            {
+                return AuthenticateResult.Fail("Invalid Authorization Header");
+            }
+        }
+    }
+}
---
+++ LearnBasicAuth/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearnBasicAuth.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        private Dictionary<string, string> userRegistered = new Dictionary<string, string>()
+        {
+            { "user1", "password1" },
+            { "user2", "password2" },
+            { "user3", "password3" }
+        };
+        [HttpGet]
+        public ActionResult<Dictionary<string, string>> Index()
+        {
+            return Ok(userRegistered);
+        }
+    }
+}
